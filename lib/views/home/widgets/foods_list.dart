405d1262d49fd0521b22ca6358597de547360e80
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2024-03-05 11:00:05 -0300

    food widget

--- lib/views/home/widgets/foods_list.dart
+++ lib/views/home/widgets/foods_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:foodly/constants/constants.dart';
 import 'package:foodly/constants/uidata.dart';
+import 'package:foodly/views/home/widgets/food_widget.dart';
 
 class FoodsList extends StatelessWidget {
   const FoodsList({super.key});
@@ -9,19 +10,17 @@ class FoodsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 210.h,
+      height: 184.h,
       padding: EdgeInsets.only(left: 12.w, top: 10.h),
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: List.generate(foods.length, (i) {
           var food = foods[i];
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 200.h,
-              width: 150.w,
-              color: kSecondary,
-            ),
+          return FoodWidget(
+            image: food['imageUrl'],
+            title: food['title'],
+            time: food['time'],
+            price: food['price'].toStringAsFixed(2),
           );
         }),
       ),
