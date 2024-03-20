7c7590adfbfebdc0a6542866d53dc7933442923c
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2024-03-20 17:48:08 -0300

    modal bottom sheet

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -21,3 +21,11 @@ double height = 825.h;
 double width = 375.w;
 
 const String appBaseUrl = "http://10.0.2.2:8000";
+
+final List<String> verificationReasons = [
+  'Real-time Updates: Get instant notifications about your order status.',
+  'Direct Communication: A verified number ensures seamless communication.',
+  'Enhanced Security: Protect your account and confirm orders securely.',
+  'Effortless Rescheduling: Easily address issues with a quick call.',
+  'Exclusive Offers: Stay in the loop for special deals and promotions.'
+];
--- lib/views/food/food_page.dart
+++ lib/views/food/food_page.dart
@@ -307,9 +307,67 @@ class _FoodPageState extends State<FoodPage> {
   Future<dynamic> showVeririficationSheet(BuildContext context) {
     return showModalBottomSheet(
       context: context,
+      backgroundColor: Colors.transparent,
+      showDragHandle: true,
       builder: (BuildContext context) {
         return Container(
           height: 500.h,
+          width: width,
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage('assets/images/restaurant_bk.png'),
+              fit: BoxFit.fill,
+            ),
+            color: kLightWhite,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12.r),
+              topRight: Radius.circular(12.r),
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(8.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 10.h,
+                ),
+                ReusableText(
+                  text: "Verify Your Phone Number",
+                  style: appStyle(18, kPrimary, FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 250.h,
+                  child: Column(
+                    children:
+                        List.generate(verificationReasons.length, (index) {
+                      return ListTile(
+                        leading: const Icon(
+                          Icons.check_circle_outline,
+                          color: kPrimary,
+                        ),
+                        title: Text(
+                          verificationReasons[index],
+                          textAlign: TextAlign.justify,
+                          style: appStyle(11, kGrayLight, FontWeight.normal),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                CustomButton(
+                  text: 'Verify Phone Number',
+                  btnHeight: 35.h,
+                  onTap: () {
+                    Get.to(() => const PhoneVerificationPage());
+                  },
+                )
+              ],
+            ),
+          ),
         );
       },
     );
