750b2e7e1624b0e00002ca11472306272af6f042
Jonathan Gontijo <jonathan.ferreira1312@hotmail.com>
2024-03-25 15:36:12 -0300

    verification controller

--- lib/views/auth/verification_page.dart
+++ lib/views/auth/verification_page.dart
@@ -6,6 +6,8 @@ import 'package:foodly/common/custom_button.dart';
 import 'package:foodly/common/custom_container.dart';
 import 'package:foodly/common/reusable_text.dart';
 import 'package:foodly/constants/constants.dart';
+import 'package:foodly/controllers/verification_controller.dart';
+import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 
@@ -14,79 +16,84 @@ class VerificationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(VerificationController());
     return Scaffold(
-        appBar: AppBar(
-          title: ReusableText(
-            text: 'Please Verify Your Account ',
-            style: appStyle(
-              12,
-              kGray,
-              FontWeight.w600,
+      appBar: AppBar(
+        title: ReusableText(
+          text: 'Please Verify Your Account ',
+          style: appStyle(
+            12,
+            kGray,
+            FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: CustomContainer(
+        color: Colors.white,
+        containerContent: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.w),
+          child: SizedBox(
+            height: height,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                Lottie.asset("assets/anime/delivery.json"),
+                SizedBox(
+                  height: 30.h,
+                ),
+                ReusableText(
+                  text: "Verify your account",
+                  style: appStyle(
+                    20,
+                    kPrimary,
+                    FontWeight.w600,
+                  ),
+                ),
+                SizedBox(
+                  height: 5.h,
+                ),
+                Text(
+                  "Enter the 6 digit code sent to your email, if you don't see the code, please check your spam folder ",
+                  textAlign: TextAlign.justify,
+                  style: appStyle(
+                    10,
+                    kGray,
+                    FontWeight.normal,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                OtpTextField(
+                  numberOfFields: 6,
+                  borderColor: kPrimary,
+                  borderWidth: 2.0,
+                  textStyle: appStyle(17, kDark, FontWeight.w600),
+                  onCodeChanged: (String code) {},
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  onSubmit: (String verificationCode) {
+                    controller.setCode = verificationCode;
+                  }, // end onSubmit
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                CustomButton(
+                  text: "V E R I F Y  A C C O U N T",
+                  onTap: () {
+                    controller.verificationFunction("");
+                  },
+                  btnHeight: 35.h,
+                  btnWidth: width,
+                ),
+              ],
             ),
           ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          elevation: 0,
         ),
-        body: CustomContainer(
-            color: Colors.white,
-            containerContent: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.w),
-              child: SizedBox(
-                height: height,
-                child: ListView(
-                  padding: EdgeInsets.zero,
-                  children: [
-                    Lottie.asset("assets/anime/delivery.json"),
-                    SizedBox(
-                      height: 30.h,
-                    ),
-                    ReusableText(
-                      text: "Verify your account",
-                      style: appStyle(
-                        20,
-                        kPrimary,
-                        FontWeight.w600,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 5.h,
-                    ),
-                    Text(
-                      "Enter the 6 digit code sent to your email, if you don't see the code, please check your spam folder ",
-                      textAlign: TextAlign.justify,
-                      style: appStyle(
-                        10,
-                        kGray,
-                        FontWeight.normal,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10.h,
-                    ),
-                    OtpTextField(
-                      numberOfFields: 6,
-                      borderColor: kPrimary,
-                      borderWidth: 2.0,
-                      textStyle: appStyle(17, kDark, FontWeight.w600),
-                      onCodeChanged: (String code) {},
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      onSubmit: (String verificationCode) {
-                        print(verificationCode);
-                      }, // end onSubmit
-                    ),
-                    SizedBox(
-                      height: 10.h,
-                    ),
-                    CustomButton(
-                      text: "V E R I F Y  A C C O U N T",
-                      onTap: () {},
-                      btnHeight: 35.h,
-                      btnWidth: width,
-                    ),
-                  ],
-                ),
-              ),
-            )));
+      ),
+    );
   }
 }
